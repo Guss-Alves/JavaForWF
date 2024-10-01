fcc59773482ffdebb52d334628dae3197203fa57
Gustavo Alves Magalhaes <104655523+Guss-Alves@users.noreply.github.com>
2024-09-30 21:44:20 -0700

    git start

---
+++ javaWork/Hello.java
@@ -0,0 +1,6 @@
+public class Hello{
+    //this line main is always necessary on Java applications
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+} 
