f685b81d0d4e8e70b1d5f6c42f745b3b816ce883
Gustavo Alves Magalhaes <104655523+Guss-Alves@users.noreply.github.com>
2024-10-14 18:27:35 -0700

    first commit

--- javaWork/Hello.java
+++ javaWork/Hello.java
@@ -1,6 +1,12 @@
 public class Hello{
     //this line main is always necessary on Java applications
     public static void main(String[] args){
-        System.out.println("hello world");
+        // System.out.println("hello world");
+        String firstName = "Gustavo";
+        String lastName = "Alves";
+
+        String result = firstName + " " + lastName;
+        System.out.println(result);
+        System.out.println("u got it");
     }
 } 
